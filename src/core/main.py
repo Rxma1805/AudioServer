db574e4bcdf6fc37a839db96f621967127ac64b8
Rxma1805 <ma.nuc@qq.com>
2018-04-04 17:05:58 +0800

    change process

--- src/core/main.py
+++ src/core/main.py
@@ -1,19 +1,11 @@
-'''
-Created on 2018年3月28日
-
-@author: Administrator
-'''
-#from gevent.server import StreamServer
-from pyaudio import paInt16,PyAudio
-import wave
+import socketserver
 import time
 import os
-#import socket
+import wave
 import threading
-#from multiprocessing import process
-import socketserver
 
-
+DictAudioClient={}
+DictUserClient={}
 channels = 1
 frameRate=8000
 secondLong=60
@@ -24,129 +16,121 @@ audioCmdPort = 12345
 audioFilePort = 12347
 userPort = 12348
 
-DictAudioClient={}
-DictUserClient={}    
+class AudioFile(socketserver.BaseRequestHandler):
+    def get_time_now(self):
+        end = time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
+        start = time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()-secondLong))
+        return start,end
     
-    
-def get_time_now():
-    end = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-    start = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()-secondLong))
-    return start,end
-
-def save_wave(data,path,start,end):   
-    #start,end = get_time_now()
-    fileName = path+"/"+start+'_'+end+'.wav'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if not os.path.exists(fileName):
-        os.system(r'touch %s' % fileName)
-    datas=[]
-    wf=wave.open(fileName,'rb')
-    datas.append(wf.readframes(wf.getnframes()))
-    wf.close()
-    datas.append(data)
-    wf=wave.open(fileName,'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(2)
-    wf.setframerate(frameRate)    
-    wf.writeframes(datas.append(datas))
-    wf.close()
+    def save_mp3(self,data,path,start,end):   
+        #start,end = get_time_now()
+        fileName = path+"/"+start+'_'+end+'.mp3'
+        oldData=b''
+        if not os.path.exists(path):
+            os.makedirs(path)
+#         if os.path.exists(fileName):
+#             wf=wave.open(fileName,'rb')        
+#             oldData = wf.readframes(wf.getnframes())
+#             wf.close() 
+#                    
+#         wf=wave.open(fileName,'wb')
+#         wf.setnchannels(channels)
+#         wf.setsampwidth(2)
+#         wf.setframerate(frameRate)  
+#         if(len(oldData)>0):
+#             wf.writeframes(oldData)              
+#         wf.writeframes(data)
+#         wf.close()
+#         if os.path.exists(fileName):
+#             wf=wave.open(fileName,'rb')        
+#             oldData = wf.readframes(wf.getnframes())
+#             wf.close() 
+                   
+        f=open(fileName,'ab+')
+        f.write(data)
+        f.close()
         
-def audio_file_save(sock,address): 
-    start=""
-    end=""
-    print("audio file received connect %s" % address)
-    while True:        
-        data = sock.recv(2054).decode()
-        if(not data):
-            print("client disconnected! %s" % address)
-            break
-        elif data == "OK":
-            sock.send(b"OK")
-            start,end = get_time_now()
-        elif data.find("DATA"):
-            save_wave(data.split('_')[1],'../record',start,end)
-        elif data.find("END"):
-            continue
-        else:
-            continue
-               
-def audio_cmd_handle(sock,address):
-    DictAudioClient[address]=sock
-    print('audio cmd connect % s' % address)
-        
-    while True:
-        data = sock.recv(2048)
-        if(not data):
-            print("audio cmd client disconnected! %s" % address)
-            DictAudioClient.pop(address)
-            break
-        elif(data.find("CMD".encode(encoding='utf_8', errors='strict'))):#CMD_START_DESTIP_SRCIP
-            srcIP = data.split('_')[3]
-            DictUserClient[srcIP].send(data)
-        elif(data.find("DATA".encode(encoding='utf_8', errors='strict'))):#DATA_SRCIP_dddd
-            srcIP = data.split('_')[1]#if use rtsp  then here need change
-            DictUserClient[srcIP].send(data.split('_')[2])
-        else:
-            continue
-
-def user_cmd_handle(sock,address):
-    DictUserClient[address]=sock
-    print('New user connect % s'% address)
-    
-    while True:
-        data = sock.recv(2048)
-        if(not data):
-            print("user client disconnected! % s" % address)
-            DictUserClient.pop(address)
-            break
-        elif(data.find("CMD".encode(encoding='utf_8', errors='strict'))):
-            #CMD_START_DESTIP_SRCIP            
-            destIP = data.split('_')[2]
-            if(DictAudioClient.has_key(destIP)):
-                DictAudioClient[destIP].send(data)
+    def handle(self):
+        self.start=""
+        self.end=""        
+        print("audio file received connect %s" % self.client_address[0])
+        while True:        
+#             data = self.request.recv(2054).decode()
+            data = self.request.recv(3072)
+            if(not data):
+                print("client disconnected! %s" % self.client_address[0])
+                break
+            elif data[0:4] == "SEND".encode():
+                self.request.send(b"OK")
+                self.start,self.end = self.get_time_now()
+            elif data[0:4] == "DATA".encode():
+                self.save_mp3(data[4:],os.path.abspath('..')+'/record/%s' % self.client_address[0],self.start,self.end)
+            elif data[0:4] == "1END".encode():
+                continue
             else:
-                print("User want to connect %s but it didn't work!" % destIP)
-        else:
-            continue
- 
+                continue
+class AudioCmd(socketserver.BaseRequestHandler):
+    def handle(self):
+        DictAudioClient[self.client_address[0]]=self.request
+        print('audio cmd connect % s' % self.client_address[0])
+            
+        while True:
+            data = self.request.recv(2048)
+            if(not data):
+                print("audio cmd client disconnected! %s" % self.client_address[0])
+                DictAudioClient.pop(self.client_address[0])
+                break
+            elif(data[0:3] == "CMD".encode()):#CMD_START_DESTIP_SRCIP
+                srcIP = data.decode().split('_')[3]
+                DictUserClient[srcIP].send(data)
+            elif data[0:3] == "DAT".encode():#DATA_SRCIP_dddd
+                start_index = 5#find second'_'
+                for c in data[5:]:                    
+                    if chr(c) == '_':
+                        break
+                    start_index+=1
+                srcIP = data[5:start_index].decode()#if use rtsp  then here need change
+                if(srcIP in DictUserClient):
+                    DictUserClient[srcIP].send(data[start_index+1:])
+                else:
+                    print("Device want to connect %s but the user didn't work!" % srcIP)
+            else:
+                continue
 
-def server_start(server):
-    server.serve_forever()
-                  
+class UserCmd(socketserver.BaseRequestHandler):
+    def handle(self):
+        DictUserClient[self.client_address[0]]=self.request
+        print('New user connect % s'% self.client_address[0])
+        
+        while True:
+            data = self.request.recv(2048).decode()
+            if(not data):
+                print("user client disconnected! % s" % self.client_address[0])
+                DictUserClient.pop(self.client_address[0])
+                break
+            elif(data.find("CMD") == 0):
+                #CMD_START_DESTIP_SRCIP            
+                destIP = data.split('_')[2]
+                if(destIP in DictAudioClient):
+                    DictAudioClient[destIP].send(data.encode())
+                else:
+                    print("User want to connect %s but it didn't work!" % destIP)
+            else:
+                continue
+            
 if __name__ == "__main__":
-    
-    socketserver.TCPServer((serverIP,audioFilePort),audio_file_save)
-    
-    
-    
-    
-    server = StreamServer((serverIP,audioFilePort),audio_file_save)
-    #server.daemo = True
-    #server.serve_forever()
-    t1 = threading.Thread(target = server_start,args=(server,))
-    t1.daemon=True
+    AudioFileServer = socketserver.ThreadingTCPServer((serverIP,audioFilePort),AudioFile)
+    t1 = threading.Thread(target=AudioFileServer.serve_forever)
     t1.start()
     
-    
-    cmdServer = StreamServer((serverIP,audioCmdPort),audio_cmd_handle)
-    #cmdServer.serve_forever()#
-    t2 = threading.Thread(target = server_start,args=(cmdServer,))
-    t2.daemon = True
+    AudioCmdServer = socketserver.ThreadingTCPServer((serverIP,audioCmdPort),AudioCmd)
+    t2 = threading.Thread(target=AudioCmdServer.serve_forever)
     t2.start()
     
-    userServer = StreamServer((serverIP,userPort),user_cmd_handle)
-    #userServer.serve_forever()#
-    #t3 = threading.Thread(target = server_start,args=(userServer,))
-#       
-#     t1.start()
-#     t2.start()
+    UserCmdServer = socketserver.ThreadingTCPServer((serverIP,userPort),UserCmd)
+#     t3 = threading.Thread(target=UserCmdServer.serve_forever)
 #     t3.start()
-#     t3 = threading.Thread(target=userServer.serve_forever)
-#     t3.start()
-   
-
+    UserCmdServer.serve_forever()
     
-
-        
-     
+    
+    
--- src/core/test.py
+++ src/core/test.py
@@ -6,16 +6,19 @@ Created on 2018年3月29日
 import os
 import wave
 from pyaudio import PyAudio,paInt16
+import time
+import socket
+def get_time_now():
+    end = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    start = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()-5))
+    return start,end
+    
+    
+IP='192.168.2.19'
+PORT = 12348
 
-data=[]
-fileName = '1.wav'
-wf=wave.open(fileName,'rb')
-data.append(wf.readframes(wf.getnframes()))
-wf.close()
-
-wf=wave.open(fileName,'wb')
-wf.setnchannels(1)
-wf.setsampwidth(2)
-wf.setframerate(8000)
-wf.writeframes(data.append(data))
-wf.close()
+tcp_clent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_clent.connect((IP,PORT))
+tcp_clent.send("CMD_START_192.168.2.118_192.168.2.19".encode(encoding='utf_8', errors='strict'))
+while True:
+    pass
